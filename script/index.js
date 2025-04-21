addb53f005ee3125eb9f5fa226e5f3f3a66292d4
macbookpro <kakakda772003@gmail.com>
2025-04-21 22:38:21 +0700

    update-all-code

--- script/index.js
+++ script/index.js
@@ -1,11 +1,12 @@
 $(document).ready(() => new Controller());
-    
+
 class Controller {
     constructor() {
         this.view = new View();
         this.data = new Data();
         this.data1 = new Data();
         this.data2 = new Data();
+        this.data3 = new Data();
         this.BindEvent();
     }
 
@@ -14,7 +15,8 @@ class Controller {
         const allProducts = [
             ...this.data.data,
             ...this.data1.data1,
-            ...this.data2.data2
+            ...this.data2.data2,
+            ...this.data3.data3
         ];
         this.view.drawCard(allProducts);
 
@@ -23,7 +25,8 @@ class Controller {
             const all = [
                 ...this.data.data,
                 ...this.data1.data1,
-                ...this.data2.data2
+                ...this.data2.data2,
+                ...this.data3.data3
             ];
             this.view.drawCard(all);
         });
@@ -45,175 +48,101 @@ class Controller {
             const kids = this.data2.data2.filter(v => v.Type.toLowerCase() === "kids");
             this.view.drawCard(kids);
         });
+
+        // UNISEX
+        this.view.addEventClickOnBtnUnisex(e => {
+            const unisex = this.data3.data3.filter(v => v.Type.toLowerCase() === "unisex");
+            this.view.drawCard(unisex);
+        });
     }
 }
 
+class View {
+    constructor() {
+        this.data = new Data();
+        this.data1 = new Data();
+        this.data2 = new Data();
+        this.data3 = new Data();
+    }
 
-    class View {
-        constructor() {
-            this.data = new Data();;
-            this.data1 = new Data();;
-            this.data2 = new Data();;
-
-        }
-
-        drawCard(data) {
-            let html = "";
-            data.map(v => {
-                html += `
-                    <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12  mt-4" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="4500">
-                        <div class="card">
-                            <img src="${v.Image}" class="card-img-top" alt="">
-                        </div>
+    drawCard(data) {
+        let html = "";
+        data.map(v => {
+            html += `
+                <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 mt-4" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="4500">
+                    <div class="card">
+                        <img src="${v.Image}" class="card-img-top" alt="">
                     </div>
-                `
-            })
-
-            $(".draw-card").html(html);
-        }
-
-
-        addEventClickOnBtnAll(callBack) {
-            $("#all").click(e => callBack(e));
-        }
-
-        addEventClickOnBtnMale(callBack) {
-            $("#male").click(e => callBack(e));
-        }
-
-        addEventClickOnBtnFemale(callBack) {
-            $("#female").click(e => callBack(e));
-        }
-        addEventClickOnBtnKids(callBack) {
-            $("#kids").click(e => callBack(e));
-        }
+                </div>
+            `;
+        });
+        $(".draw-card").html(html);
     }
 
-    class Data {
-        constructor() {
-            this.data = [
-                {   
-                    Type : "male",
-                    Image: "./Picture/Coffee-1.jpg"
-                },
-                {
-                    Type : "male",
-                    Image: "./Picture/Coffee-2.jpg"
-                },
-                {
-                    Type : "male",
-                    Image: "./Picture/Coffee-3.jpg"
-                },
-                {
-                    Type : "male",
-                    Image: "./Picture/Coffee-4.jpg"
-                },
-                {
-                    Type : "male",
-                    Image: "./Picture/Coffee-5.jpg"
-                },
-                {
-                    Type : "male",
-                    Image: "./Picture/Coffee-6.jpg"
-                },
-                {
-                    Type : "male",
-                    Image: "./Picture/Coffee-7.jpg"
-                },
-                {
-                    Type : "male",
-                    Image: "./Picture/Coffee-8.jpg"
-                },
-                {
-                    Type : "male",
-                    Image: "./Picture/Coffee-9.jpg"
-                },
-                {
-                    Type : "male",
-                    Image: "./Picture/Coffee-10.jpg"
-                },
-                {
-                    Type : "male",
-                    Image: "./Picture/Coffee-11.jpg"
-                },
-            ]
-        this.data1 = [
-            {
-                Type : "female",
-                Image: "./Picture/Burger-1.jpg"
-            },
-            {
-                Type : "female",
-                Image: "./Picture/Burger-2.jpg"
-            },
-            {
-                Type : "female",
-                Image: "./Picture/Burger-3.jpg"
-            },
-            {
-                Type : "female",
-                Image: "./Picture/Burger-4.jpg"
-            },
-            {
-                Type : "female",
-                Image: "./Picture/Burger-5.jpg"
-            },
-            {
-                Type : "female",
-                Image: "./Picture/Burger-6.jpg"
-            },
-            {
-                Type : "female",
-                Image: "./Picture/Burger-7.jpg"
-            },    
-        ]
-        this.data2 = [
-            {
-                Type : "kids",
-                Image: "./Picture/Picture-1.jpg"
-            },
-            {
-                Type : "kids",
-                Image: "./Picture/Picture-2.jpg"
-            },
-            {
-                Type : "kids",
-                Image: "./Picture/Picture-3.jpg"
-            },
-            {
-                Type : "kids",
-                Image: "./Picture/Picture-4.jpg"
-            },
-            {
-                Type : "kids",
-                Image: "./Picture/Picture-5.jpg"
-            },
-            {
-                Type : "kids",
-                Image: "./Picture/Picture-6.jpg"
-            },
-            {
-                Type : "kids",
-                Image: "./Picture/Picture-7.jpg"
-            },
-            {
-                Type : "kids",
-                Image: "./Picture/Picture-8.jpg"
-            },
-            {
-                Type : "kids",
-                Image: "./Picture/Picture-9.jpg"
-            },
-            {
-                Type : "kids",
-                Image: "./Picture/Shoes-1.jpg"
-            },
-            {
-                Type : "kids",
-                Image: "./Picture/Shoes-2.jpg"
-            },
-        ]
+    addEventClickOnBtnAll(callBack) {
+        $("#all").click(e => callBack(e));
+    }
+
+    addEventClickOnBtnMale(callBack) {
+        $("#male").click(e => callBack(e));
+    }
+
+    addEventClickOnBtnFemale(callBack) {
+        $("#female").click(e => callBack(e));
+    }
+
+    addEventClickOnBtnKids(callBack) {
+        $("#kids").click(e => callBack(e));
+    }
+
+    addEventClickOnBtnUnisex(callBack) {
+        $("#unisex").click(e => callBack(e));
     }
 }
 
+class Data {
+    constructor() {
+        this.data = [
+            { Type: "male", Image: "./Picture/Coffee-1.jpg" },
+            { Type: "male", Image: "./Picture/Coffee-2.jpg" },
+            { Type: "male", Image: "./Picture/Coffee-3.jpg" },
+            { Type: "male", Image: "./Picture/Coffee-4.jpg" },
+            { Type: "male", Image: "./Picture/Coffee-5.jpg" },
+            { Type: "male", Image: "./Picture/Coffee-6.jpg" },
+            { Type: "male", Image: "./Picture/Coffee-7.jpg" },
+            { Type: "male", Image: "./Picture/Coffee-8.jpg" },
+            { Type: "male", Image: "./Picture/Coffee-9.jpg" },
+            { Type: "male", Image: "./Picture/Coffee-10.jpg" },
+            { Type: "male", Image: "./Picture/Coffee-11.jpg" },
+        ];
 
+        this.data1 = [
+            { Type: "female", Image: "./Picture/Burger-1.jpg" },
+            { Type: "female", Image: "./Picture/Burger-2.jpg" },
+            { Type: "female", Image: "./Picture/Burger-3.jpg" },
+            { Type: "female", Image: "./Picture/Burger-4.jpg" },
+            { Type: "female", Image: "./Picture/Burger-5.jpg" },
+            { Type: "female", Image: "./Picture/Burger-6.jpg" },
+            { Type: "female", Image: "./Picture/Burger-7.jpg" },
+        ];
+
+        this.data2 = [
+            { Type: "kids", Image: "./Picture/Picture-1.jpg" },
+            { Type: "kids", Image: "./Picture/Picture-2.jpg" },
+            { Type: "kids", Image: "./Picture/Picture-3.jpg" },
+            { Type: "kids", Image: "./Picture/Picture-4.jpg" },
+            { Type: "kids", Image: "./Picture/Picture-5.jpg" },
+            { Type: "kids", Image: "./Picture/Picture-6.jpg" },
+            { Type: "kids", Image: "./Picture/Picture-7.jpg" },
+            { Type: "kids", Image: "./Picture/Picture-8.jpg" },
+            { Type: "kids", Image: "./Picture/Picture-9.jpg" },
+            { Type: "kids", Image: "./Picture/Shoes-1.jpg" },
+            { Type: "kids", Image: "./Picture/Shoes-2.jpg" },
+        ];
+
+        this.data3 = [
+            { Type: "unisex", Image: "./Picture/travel.jpg" },
+            
+        ];
+    }
+}
